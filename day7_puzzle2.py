7e7226d0073b8c16f7b56ef9b00aff11e9ce4452
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-12-07 12:58:49 -0500

    Puzzle 2 solution can solve puzzle 1.

--- day7_puzzle2.py
+++ day7_puzzle2.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from sys import argv
+from sys import argv, exit
 
 from data_reading import\
 	convert_list_content,\
@@ -9,7 +9,17 @@ from data_reading import\
 _COMMA = ","
 
 
-def _fuel_cost(alignment, positions):
+def _fuel_cost_puzzle1(alignment, positions):
+	cost = 0
+
+	for position in positions:
+		distance = abs(alignment - position)
+		cost += distance
+
+	return cost 
+
+
+def _fuel_cost_puzzle2(alignment, positions):
 	cost = 0
 
 	for position in positions:
@@ -20,15 +30,24 @@ def _fuel_cost(alignment, positions):
 
 
 data_path = Path(argv[1])
+puzzle_num = int(argv[2])
 
 positions = data_from_lines(data_path)[0].split(_COMMA)
 convert_list_content(positions, int)
+
+if puzzle_num == 1:
+	fuel_cost_fnc = _fuel_cost_puzzle1
+elif puzzle_num == 2:
+	fuel_cost_fnc = _fuel_cost_puzzle2
+else:
+	print(f"{puzzle_num} is not a puzzle number.")
+	exit(1)
 
 min_fuel_cost = float("inf")
 min_fuel_cost_pos = -1
 
 for position in range(min(positions), max(positions)+1):
-	cost = _fuel_cost(position, positions)
+	cost = fuel_cost_fnc(position, positions)
 
 	if cost < min_fuel_cost:
 		min_fuel_cost = cost
