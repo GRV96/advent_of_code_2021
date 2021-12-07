921335448385cd7bc9c37883ec97ad2242671961
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-12-07 00:46:01 -0500

    Puzzle 1 solved with the median
    Answer: 359648

--- day7_puzzle1.py
+++ day7_puzzle1.py
@@ -1,5 +1,6 @@
 from math import floor
 from pathlib import Path
+from statistics import median
 from sys import argv
 
 from data_reading import\
@@ -20,13 +21,12 @@ data_path = Path(argv[1])
 positions = data_from_lines(data_path)[0].split(_COMMA)
 convert_list_content(positions, int)
 
-average_position = sum(positions)/len(positions)
-average_position = int(_round_half_up(average_position))
+median_position = int(_round_half_up(median(positions)))
 
 fuel_cost = 0
 
 for position in positions:
-	fuel_cost += abs(position - average_position)
+	fuel_cost += abs(position - median_position)
 
-print(f"Alignement position: {average_position}")
+print(f"Alignement position: {median_position}")
 print(f"Fuel cost: {fuel_cost}")
