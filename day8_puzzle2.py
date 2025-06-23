424d97d58aad7aa375dca5c1df7cafdcc76613b4
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-06-22 21:52:12 -0400

    Day 8 Puzzle 2 wrong demo answer: -6860

--- day8_puzzle2.py
+++ day8_puzzle2.py
@@ -18,172 +18,171 @@ _NORMAL_DISPLAYS = {
 	6: "abdefg",
 	7: "acf",
 	8: "abcdefg",
-	9: "abcdfg"}
+	9: "abcdfg"
+}
 
-_SEG_COUNT_DIGITS = {
-	2: (1,),
-	3: (7,),
-	4: (4,),
-	5: (2, 3, 5),
-	6: (0, 6, 9),
-	7: (8,)}
+_DIGIT_TO_PATTERN_SIZE = {
+	0: 6,
+	1: 2,
+	2: 5,
+	3: 5,
+	4: 4,
+	5: 5,
+	6: 6,
+	7: 3,
+	8: 7,
+	9: 6
+}
 
-
-def _digit_display_matches_wiring(digit, display, wiring):
-	normal_display = _NORMAL_DISPLAYS[digit]
-	expected_seg_count = len(normal_display)
-
-	seg_count = 0
-
-	for normal_segment in normal_display:
-		possible_wirings = wiring[normal_segment]
-
-		for segment in possible_wirings:
-
-			if segment in display:
-				seg_count += 1
-
-			else:
-				# The display does not match the normal display.
-				return -1
-
-	if seg_count == expected_seg_count:
-		# The display fully matches the normal display.
-		return 1
-
-	# The display partially matches the normal display.
-	return 0
+_PATTERN_SIZE_TO_DIGIT = {
+	2: 1,
+	3: 7,
+	4: 4,
+	7: 8
+}
 
 
-def _displays_and_wiring(signal_patterns):
-	signal_patterns = list(signal_patterns)
-	actual_displays = _init_actual_displays(signal_patterns)
-	wiring = _init_wiring(actual_displays)
-#	for normal, actual in wiring.items():
-#		print(normal + ": " + str(actual))
-
-	while len(signal_patterns) > 0:
-
-		for patterns in actual_displays.values():
-
-			if len(patterns) == 1:
-				try:
-					signal_patterns.remove(patterns[0])
-				except ValueError:
-					pass
-
-		for pattern in signal_patterns:
-			pattern_length = len(pattern)
-
-			if pattern_length not in _SEG_COUNT_DIGITS:
-				continue
-
-			possible_digits = _SEG_COUNT_DIGITS[pattern_length]
-
-			for digit in possible_digits:
-				possible_displays = actual_displays[digit]
-
-				if len(possible_displays) == 1:
-					continue
-
-				i = 0
-				while i < len(possible_displays):
-					display = possible_displays[i]
-					match = _digit_display_matches_wiring(digit, display, wiring)
-
-					if match < 0:
-						possible_displays.remove(display)
-
-					elif match > 0:
-						possible_displays.clear()
-						possible_displays.append()
-						i += 1
-
-	return actual_displays, wiring
+def raise_digit_error(digit: int) -> None:
+	if digit < 0 or digit > 9:
+		raise ValueError(f"Digits range from 0 to 9. Recieved {digit}")
 
 
-def _init_actual_displays(signal_patterns):
-	# Keys: digits
-	# Values: lists of possible patterns (strings)
-	actual_displays = {n: list() for n in range(10)}
+class DigitPatternMap:
+	UNDEF_DIGIT = -1
 
-	for pattern in signal_patterns:
-		pattern_length = len(pattern)
+	def __init__(self) -> None:
+		self._digit_to_pattern = dict()
+		self._pattern_to_digit = dict()
 
-		if pattern_length not in _SEG_COUNT_DIGITS:
+	def get_digit(self, pattern: str) -> int:
+		return self._pattern_to_digit.get(pattern, self.__class__.UNDEF_DIGIT)
+
+	def get_digit_to_pattern(self):
+		return dict(self._digit_to_pattern)
+
+	def get_pattern(self, digit: int) -> str:
+		raise_digit_error(digit)
+		return self._digit_to_pattern.get(digit)
+
+	def get_pattern_to_digit(self):
+		return dict(self._pattern_to_digit)
+
+	def has_digit(self, digit: int) -> bool:
+		raise_digit_error(digit)
+		return digit in self._digit_to_pattern
+
+	def has_pattern(self, pattern: str) -> bool:
+		return pattern in self._pattern_to_digit
+
+	def register(self, digit: int, pattern: str) -> None:
+		raise_digit_error(digit)
+		self._digit_to_pattern[digit] = pattern
+		self._pattern_to_digit[pattern] = digit
+
+
+class PatternCollection:
+
+	def __init__(self, patterns) -> None:
+		self._content = tuple(patterns)
+
+	def __iter__(self):
+		return iter(self._content)
+
+	def get_patterns_of_size(self, pattern_size: int):
+		return *(pattern
+		   for pattern in self._content
+		   if len(pattern) == pattern_size),
+
+
+def nb_segs_not_in_other_pattern(pattern1: str, pattern2: str) -> int:
+	"""
+	Counts the segments in the first pattern that do not appear in the second
+	pattern.
+	"""
+	count = 0
+
+	for seg in pattern1:
+		if seg not in pattern2:
+			count += 1
+
+	return count
+
+
+def map_digit_to_pattern(
+		digit_pattern_map: DigitPatternMap,
+		known_digit: int,
+		unknown_digit: int,
+		difference: int) -> None:
+	raise_digit_error(known_digit)
+	raise_digit_error(unknown_digit)
+
+	pattern_size = _DIGIT_TO_PATTERN_SIZE[unknown_digit]
+	known_pattern = digit_pattern_map.get_pattern(known_digit)
+
+	for pattern in patterns:
+
+		if len(pattern) != pattern_size\
+				or digit_pattern_map.has_pattern(pattern):
 			continue
 
-		if pattern_length == 2:
-			actual_displays[1].append(pattern)
-
-		elif pattern_length == 4:
-			actual_displays[4].append(pattern)
-
-		elif pattern_length == 3:
-			actual_displays[7].append(pattern)
-
-		elif pattern_length == 7:
-			actual_displays[8].append(pattern)
-
+		if difference >= 0:
+			diff = nb_segs_not_in_other_pattern(pattern, known_pattern)
 		else:
-			possible_digits = _SEG_COUNT_DIGITS[pattern_length]
+			diff = nb_segs_not_in_other_pattern(known_pattern, pattern)
 
-			for digit in possible_digits:
-				possible_displays = actual_displays[digit]
-				possible_displays.append(pattern)
-
-	return actual_displays
-
-
-def _init_wiring(actual_displays):
-	# Keys: normal segment characters
-	# Values: sets of possible corresponding segment characters
-	wiring = {character: set() for character in "abcdefg"}
-
-	display_of_1 = actual_displays[1][0]
-	display_of_4 = actual_displays[4][0]
-	display_of_7 = actual_displays[7][0]
-	display_of_8 = actual_displays[8][0]
-
-	display_of_4_and_7 = display_of_4 + display_of_7
-
-	wiring_a = wiring['a']
-	wiring_b = wiring['b']
-	wiring_c = wiring['c']
-	wiring_d = wiring['d']
-	wiring_e = wiring['e']
-	wiring_f = wiring['f']
-	wiring_g = wiring['g']
-
-	for segment in display_of_7:
-
-		if segment in display_of_1:
-			wiring_c.add(segment)
-			wiring_f.add(segment)
-
-		elif segment not in wiring_a:
-			wiring_a.add(segment)
-
-	for segment in display_of_4:
-
-		if segment not in display_of_1:
-			wiring_b.add(segment)
-			wiring_d.add(segment)
-
-	for segment in display_of_8:
-
-		if segment not in display_of_4_and_7:
-			wiring_e.add(segment)
-			wiring_g.add(segment)
-
-	return wiring
+		if diff == difference:
+			digit_pattern_map.register(unknown_digit, pattern)
+			break
 
 
 data_path = Path(argv[1])
 
 note_entries = data_from_lines(data_path)
-signal_patterns = note_entries[0].split(_PIPE)[0]
-signal_patterns = signal_patterns.split(_SPACE)
 
-actual_displays, wiring = _displays_and_wiring(signal_patterns)
-print(actual_displays)
+output_sum = 0
+
+for entry in note_entries:
+	split_entry = entry.split(_PIPE)
+	patterns = PatternCollection(split_entry[0].strip().split(_SPACE))
+	outputs = split_entry[1].strip().split(_SPACE)
+
+	digit_pattern_map = DigitPatternMap()
+
+	for pattern in patterns:
+		pattern_length = len(pattern)
+		digit = _PATTERN_SIZE_TO_DIGIT.get(
+			pattern_length, DigitPatternMap.UNDEF_DIGIT)
+
+		if digit != DigitPatternMap.UNDEF_DIGIT:
+			digit_pattern_map.register(digit, pattern)
+
+	# 2 has three segments that 4 does not have.
+	map_digit_to_pattern(digit_pattern_map, 4, 2, 3)
+
+	# 3 has three segments that 1 does not have.
+	map_digit_to_pattern(digit_pattern_map, 1, 3, 3)
+
+	# 5 is the last digit with five segments.
+	for pattern in patterns.get_patterns_of_size(5):
+		if not digit_pattern_map.has_pattern(pattern):
+			digit_pattern_map.register(5, pattern)
+			break
+
+	# 6 has five segments that 1 does not have.
+	map_digit_to_pattern(digit_pattern_map, 1, 6, 5)
+
+	# 9 is the last digit with six segments.
+	for pattern in patterns.get_patterns_of_size(6):
+		if not digit_pattern_map.has_pattern(pattern):
+			digit_pattern_map.register(9, pattern)
+			break
+
+	number = 0
+	digits = *(digit_pattern_map.get_digit(output) for output in outputs),
+	nb_digits = len(digits)
+	for i in range(nb_digits):
+		number += digits[i] * 10 ** (nb_digits - i - 1)
+
+	output_sum += number
+
+print(f"Output sum: {output_sum}")
