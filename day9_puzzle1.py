16478db378eb3eded832383d0e04a15d5019c474
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-06-23 11:31:23 -0400

    Puzzle 1: function `_evaluate_location`

--- day9_puzzle1.py
+++ day9_puzzle1.py
@@ -4,18 +4,21 @@ from sys import argv
 from data_reading import data_from_lines
 
 
-def _coords_are_low_point(heightmap, row, column, height):
+def _evaluate_location(heightmap, row, column):
+	height = _get_from_heightmap(heightmap, i, j)
 	neighbor_heights = (
 		_get_from_heightmap(heightmap, row-1, column), # Up
 		_get_from_heightmap(heightmap, row+1, column), # Down
 		_get_from_heightmap(heightmap, row, column-1), # Left
 		_get_from_heightmap(heightmap, row, column+1)) # Right
 
+	is_low_point = True
 	for neighbor_height in neighbor_heights:
 		if neighbor_height >= 0 and neighbor_height <= height:
-			return False
+			is_low_point = False
+			break
 
-	return True
+	return height, is_low_point
 
 
 def _get_from_heightmap(heightmap, row, column):
@@ -41,9 +44,9 @@ for i in range(len(heightmap)):
 	row = heightmap[i]
 
 	for j in range(len(row)):
-		height = _get_from_heightmap(heightmap, i, j)
+		height, is_low_point = _evaluate_location(heightmap, i, j)
 
-		if _coords_are_low_point(heightmap, i, j, height):
+		if is_low_point:
 			risk_level = height + 1
 			risk_level_sum += risk_level
 
