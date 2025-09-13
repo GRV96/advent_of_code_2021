2110cb35356578fc28a4772771f17b2f15feacf8
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-09-12 23:34:31 -0400

    [11] Day 4 puzzle 1 uses `read_file_lines`.

--- day4_puzzle1.py
+++ day4_puzzle1.py
@@ -1,17 +1,44 @@
 from pathlib import Path
 from sys import argv
+from typing import Generator
 
-from data_reading import read_bingo
-from day4_bingo_grid import MarkingResult
+from data_reading import read_file_lines
+from day4_bingo_grid import BingoGrid, MarkingResult
+
+
+_SPACE = " "
+
+
+def _line_to_numbers(line: str, separator: str) -> Generator[int, None, None]:
+	return (
+		int(number)
+		for number in line.split(separator)
+		if len(number) > 0
+	)
 
 
 data_path = Path(argv[1])
 
-numbers, grids = read_bingo(data_path)
+gen_lines = read_file_lines(data_path)
+gen_numbers = _line_to_numbers(next(gen_lines), ",")
+
+# Skip an empty line.
+next(gen_lines)
+
+grids: list[BingoGrid] = list()
+grid_lines: list[tuple[int]] = list()
+for line in gen_lines:
+	if len(line) == 0:
+		grids.append(BingoGrid(grid_lines))
+		grid_lines.clear()
+	else:
+		number_line = tuple(_line_to_numbers(line, _SPACE))
+		grid_lines.append(number_line)
+
 grid_range = range(len(grids))
 
 winning_grid_num = -1
-for number in numbers:
+for number in gen_numbers:
 
 	for i in grid_range:
 		grid = grids[i]
